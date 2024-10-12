f5b8f7a4beb4889f82556a95e1fc7f67735f3615
Heltec-Aaron-Lee <support@heltec.cn>
2024-10-12 10:58:19 +0800

    Update RGB example

--- libraries/Adafruit_NeoPixel/examples/simple/simple.ino
+++ libraries/Adafruit_NeoPixel/examples/simple/simple.ino
@@ -3,15 +3,12 @@
 // Adafruit NeoPixel library
 
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        18 // On Trinket or Gemma, suggest changing this to 1
+#define PIN        PIN_NEOPIXEL // On Trinket or Gemma, suggest changing this to 1
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 1 // Popular NeoPixel ring size
+#define NUMPIXELS 2 // Popular NeoPixel ring size
 
 // When setting up the NeoPixel library, we tell it how many pixels,
 // and which pin to use to send signals. Note that for older NeoPixel
@@ -24,23 +21,18 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
   // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
   // Any other board, you can remove this part (but no harm leaving it):
-  pinMode(33,OUTPUT);
-  digitalWrite(33,1);
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  // END of Trinket-specific code.
+  pinMode(PIN_VEXT_CTL, OUTPUT);
+  digitalWrite(PIN_VEXT_CTL, VEXT_ENABLE);
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 }
 
 void loop() {
-  pixels.clear(); // Set all pixel colors to 'off'
-
   // The first NeoPixel in a strand is #0, second is 1, all the way up
   // to the count of pixels minus one.
   for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
-
+    pixels.clear(); // Set all pixel colors to 'off'
+    
     // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
     // Here we're using a moderately bright green color:
     pixels.setPixelColor(i, pixels.Color(0, 150, 0));
